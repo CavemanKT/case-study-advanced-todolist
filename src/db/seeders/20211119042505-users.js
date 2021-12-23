2e5e84a8c3430c503fbf283f4d97b86f2af3295b
caveman <vkoubbeago@gmail.com>
2021-12-23 15:27:19 +0800

    a

--- src/db/seeders/20211119042505-users.js
+++ src/db/seeders/20211119042505-users.js
@@ -1,6 +1,42 @@
 const bcrypt = require('bcrypt')
+const { Faker } = require('fakergem')
 
 const { User, AuthenticityToken, Todo, TodoItem } = require('../models')
+
+// generate a time stamp for createdAt
+const genRandDay = () => {
+  const randDay = Math.floor(Math.random() * 27) + 1
+  return randDay
+}
+
+const genRandMonth = () => {
+  const randMonth = Math.floor(Math.random() * 12) + 1
+  if (randMonth > 12) {
+    return genRandMonth()
+  }
+  return randMonth
+}
+
+const genRandYear = () => {
+  const randNum = Math.floor(Math.random() * 2)
+  switch (randNum) {
+    case 0:
+      return 2021
+    case 1:
+      return 2022
+    default:
+      return 2021
+  }
+}
+
+const genRandDate = () => {
+  const randDay = genRandDay()
+  const randMonth = genRandMonth()
+  const randYear = genRandYear()
+
+  const randDate = `${randYear}-${randMonth}-${randDay} 13:29:29.516+08`
+  return randDate
+}
 
 module.exports = {
   up: async () => {
@@ -8,11 +44,26 @@ module.exports = {
     await AuthenticityToken.destroy({ truncate: true })
     await Todo.destroy({ truncate: true })
     await TodoItem.destroy({ truncate: true })
-    const passwordHash = await bcrypt.hash('123456', 10)
+    const passwordHash = await bcrypt.hash('123123', 10)
     await User.create({
-      name: 'Tester',
+      name: `${Faker.Name.firstName()} ${Faker.Name.lastName()}`,
       email: '1@test.com',
-      passwordHash
+      passwordHash,
+      registrationType: 'email'
     })
+    for (let i = 1; i <= 4; i++) {
+      await Todo.create({
+        name: `List ${i}`
+      })
+      for (let j = 1; j <= 4; j++) {
+        await TodoItem.create({
+          name: `Task ${j}`,
+          description: Faker.Lorem.sentence(4),
+          complete: false,
+          deadline: genRandDate(),
+          TodoId: i
+        })
+      }
+    }
   }
 }
